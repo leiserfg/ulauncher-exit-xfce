2fcab42e69ccfb083872e4f1a40bc8dd9baa1ee0
leiserfg <leiserfg@gmail.com>
2017-11-01 15:28:08 -0400

    Change implementation

--- main.py
+++ main.py
@@ -1,63 +1,46 @@
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.RenderResultListAction import (
+    RenderResultListAction
+)
 from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
-import os
 
-class CinnamonSessionExtension(Extension):
 
+class XFCESessionExtension(Extension):
     def __init__(self):
-        super(CinnamonSessionExtension, self).__init__()
+        super(XFCESessionExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
 
+items_cache = [
+    ("reboot", ExtensionResultItem(
+        icon='images/reboot.png',
+        name='Reboot',
+        description='Reboot computer',
+        on_enter=RunScriptAction("xfce4-session-logout --reboot", None))),
+    ("shutdown", ExtensionResultItem(
+                                 icon='images/shutdown.png',
+                                 name='Shutdown',
+                                 description='Power off computer',
+                                 on_enter=RunScriptAction(
+                                     "xfce4-session-logout --halt", None))),
+    ("logout", ExtensionResultItem(
+        icon='images/logout.png',
+        name='Logout',
+        description='Logout from session',
+        on_enter=RunScriptAction("xfce4-session-logout --logout", None)))
+]
+
+
 class KeywordQueryEventListener(EventListener):
-
     def on_event(self, event, extension):
-        items = []
-    	options = ['logout', 'restart', 'reboot', 'shutdown', 'halt']
-        myList = event.query.split(" ")
-        if len(myList) == 1:
-            items.append(ExtensionResultItem(icon='images/reboot.png',
-                                            name='Reboot',
-                                            description='Reboot computer',
-                                            on_enter=RunScriptAction("cinnamon-session-quit --reboot", None)))
-            items.append(ExtensionResultItem(icon='images/shutdown.png',
-                                            name='Shutdown',
-                                            description='Power off computer',
-                                            on_enter=RunScriptAction("cinnamon-session-quit --power-off", None)))
-            items.append(ExtensionResultItem(icon='images/logout.png',
-                                            name='Logout',
-                                            description='Logout from session',
-                                            on_enter=RunScriptAction("cinnamon-session-quit --logout", None)))
 
-            return RenderResultListAction(items)
-        else:
-            myQuery = myList[1]
-            included = []
-            for option in options:
-                if myQuery in option:
-                    if option in ['shutdown', 'halt'] and 'shutdown' not in included:
-                        items.append(ExtensionResultItem(icon='images/shutdown.png',
-                                                        name='Shutdown',
-                                                        description='Power off computer',
-                                                        on_enter=RunScriptAction("cinnamon-session-quit --power-off", None)))
-                        included.append('shutdown')
-                    elif option in ['restart', 'reboot'] and 'reboot' not in included:
-                        items.append(ExtensionResultItem(icon='images/reboot.png',
-                                                    name='Reboot',
-                                                    description='Reboot computer',
-                                                    on_enter=RunScriptAction("cinnamon-session-quit --reboot", None)))
-                        included.append('reboot')
-                    elif option in ['logout']:
-                        items.append(ExtensionResultItem(icon='images/logout.png',
-                                                    name='Logout',
-                                                    description='Logout from session',
-                                                    on_enter=RunScriptAction("cinnamon-session-quit --logout", None)))
+        term = (event.get_argument() or "").lower()
+        items = [i for name, i in items_cache if name.startswith(term)]
+        return RenderResultListAction(items)
 
-            return RenderResultListAction(items)
 
 if __name__ == '__main__':
-    CinnamonSessionExtension().run()
+    XFCESessionExtension().run()
